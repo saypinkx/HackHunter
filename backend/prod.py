b751c7bd710f5c4c7e1a5de55aee8f21a2802ce2
sixzerx <paprut538@gmail.com>
2024-04-21 18:49:54 +0300

    finally prod

--- backend/prod.py
+++ backend/prod.py
@@ -2,22 +2,14 @@ import os
 import subprocess
 import time
 
-# Отслеживание изменений в репозитории с помощью Git
-while True:
 
-    # Проверка наличия нового коммита
+while True:
     new = str(subprocess.check_output(["git", "pull", "origin", "master"]))
     if "Already up to date" not in new:
-        # Есть новый коммит
         print("Обнаружен новый коммит. Применяем изменения...")
         print(new)
-        time.sleep(5)
-        # Применение новых коммитов
-        # Пересборка Docker Compose
         print("Пересобираем Docker Compose...")
         subprocess.check_call(["docker-compose", "up", "-d", "--no-deps", "--build"])
     else:
         print("Нет новых коммитов. Ожидание...")
-
-    # Ожидание изменений в репозитории
     time.sleep(5)
