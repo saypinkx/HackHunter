0f80de15cad2e372b7e229b8fc61002d8d25fe96
sixzerx <paprut538@gmail.com>
2024-04-21 18:04:10 +0300

    edit prod

--- backend/prod.py
+++ backend/prod.py
@@ -4,19 +4,17 @@ import time
 
 # Отслеживание изменений в репозитории с помощью Git
 while True:
-    # Получение текущего хеша коммита
-    current_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+
 
     # Проверка наличия нового коммита
-    new_commits = subprocess.check_output(["git", "fetch", "--all"]).decode("utf-8")
-    if current_commit not in new_commits and new_commits is not None:
+    new =subprocess.call(["git", "pull", "origin", "master"])
+    if new:
 
         # Есть новый коммит
-        print(f'current:{current_commit}, new:{new_commits}')
         print("Обнаружен новый коммит. Применяем изменения...")
         time.sleep(5)
         # Применение новых коммитов
-        subprocess.check_call(["git", "pull", "origin", "master"])
+
 
 
         # Пересборка Docker Compose
