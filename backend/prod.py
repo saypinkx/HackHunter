d0dff19389976f82b1743c5490430378d44d3574
sixzerx <paprut538@gmail.com>
2024-04-21 17:35:39 +0300

    edit prod

--- backend/prod.py
+++ backend/prod.py
@@ -8,13 +8,14 @@ while True:
     current_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
 
     # Проверка наличия нового коммита
-    new_commits = subprocess.check_output(["git", "fetch"]).decode("utf-8")
-    if current_commit not in new_commits:
+    new_commits = subprocess.check_output(["git", "fetch", "--all"]).decode("utf-8")
+    if new_commits:
+        if current_commit not in new_commits:
         # Есть новый коммит
-        print("Обнаружен новый коммит. Запуск сборки Docker Compose...")
+            print("Обнаружен новый коммит. Запуск сборки Docker Compose...")
 
         # Запуск сборки Docker Compose
-        subprocess.check_call(["docker-compose", "up", "-d", "--no-deps", "--build"])
+            subprocess.check_call(["docker-compose", "up", "-d", "--no-deps", "--build"])
 
     # Ожидание изменений в репозитории
     time.sleep(5)
