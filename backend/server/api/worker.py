1c79e10c009c4bfc820fe31df9dd58d380592d51
sixzerx <paprut538@gmail.com>
2024-05-08 21:45:57 +0300

    try celery

--- backend/server/api/worker.py
+++ backend/server/api/worker.py
@@ -1,13 +1,19 @@
 from celery import Celery
-from buckets.avatar import session
+import boto3
+from config import aws_access_key_id, aws_secret_access_key
+
 from config import REDIS_NAME, REDIS_PORT, REDIS_HOST
 
 celery = Celery('tasks', broker=F'redis://62.113.104.103:6379',
                 backend=F'redis://62.113.104.103:6379')
 
-
+storage_session_base = boto3.session.Session().client(service_name='s3',
+                                                     endpoint_url='https://storage.yandexcloud.net',
+                                                     region_name='ru-central1',
+                                                     aws_access_key_id=aws_access_key_id,
+                                                     aws_secret_access_key=aws_secret_access_key)
 @celery.task
 def load_to_storage(file_content, bucket_name, filename):
-    storage_session = session
+    storage_session = storage_session_base
     response = storage_session.put_object(Bucket=bucket_name, Key=filename, Body=file_content)
     return response
