a32b202788a63b6065afe02b54bcd5391d14530e
sixzerx <paprut538@gmail.com>
2024-05-08 21:25:34 +0300

    idt dc:

--- backend/server/api/worker.py
+++ backend/server/api/worker.py
@@ -2,8 +2,8 @@ from celery import Celery
 from buckets.avatar import session
 from config import REDIS_NAME, REDIS_PORT, REDIS_HOST
 
-celery = Celery('tasks', broker=F'{REDIS_NAME}://{REDIS_HOST}:{REDIS_PORT}',
-                backend=F'{REDIS_NAME}://{REDIS_HOST}:{REDIS_PORT}')
+celery = Celery('tasks', broker=F'redis://62.113.104.103:6379',
+                backend=F'redis://62.113.104.103:6379')
 
 
 @celery.task
