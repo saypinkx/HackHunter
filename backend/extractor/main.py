79066a25d6e0a34cf45671dbce940f3c1d14f66f
sixzerx <paprut538@gmail.com>
2024-06-08 17:18:29 +0300

    add extractor

--- backend/extractor/main.py
+++ backend/extractor/main.py
@@ -13,7 +13,9 @@ def get_links(url):
     url = url
     links = []
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     driver.implicitly_wait(2)
