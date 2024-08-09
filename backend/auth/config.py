0c7f200e166aeeae3525ad041d50e5446cbd49a8
sixzerx <paprut538@gmail.com>
2024-08-09 20:49:37 +0300

    add base auth and add auth_mongo

---
+++ backend/auth/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+TOKEN_EXPIRE_MINUTES = os.environ.get("TOKEN_EXPIRE")
+
